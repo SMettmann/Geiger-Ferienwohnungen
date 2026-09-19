707199b59fc36e46d65d40b63d7a44a0aedbeb67
SMettmann <Sven.mettmann88@web.de>
2026-09-19 12:31:34 +0200

    Add Zurich lightbox slideshow

--- script.js
+++ script.js
@@ -7,18 +7,30 @@ menuButton?.addEventListener('click',()=>{
 nav?.querySelectorAll('a').forEach(a=>a.addEventListener('click',()=>nav.classList.remove('open')));
 document.getElementById('year').textContent=new Date().getFullYear();
 
-const gallery=[
-  ['https://a0.muscache.com/im/pictures/hosting/Hosting-1640774749513849269/original/aaef4c64-34a1-4601-b5ea-7457669fb338.jpeg?im_w=1200','Wohnbereich'],
-  ['https://a0.muscache.com/im/pictures/hosting/Hosting-1640774749513849269/original/45ea1953-9627-4e41-840e-2433be61feb4.jpeg?im_w=1200','Essbereich'],
-  ['https://a0.muscache.com/im/pictures/hosting/Hosting-1640774749513849269/original/2455e184-3d05-4c2d-9c07-9c8064c618d5.jpeg?im_w=1200','Küche'],
-  ['https://a0.muscache.com/im/pictures/hosting/Hosting-1640774749513849269/original/41b3c36f-6c76-4afd-8bdb-e488a646fff0.png?im_w=1440','Schlafzimmer mit Seeblick'],
-  ['https://a0.muscache.com/im/pictures/hosting/Hosting-1640774749513849269/original/4b0a0c6d-473b-46a3-9b7b-62c8b3e9e60a.jpeg?im_w=1200','Weiteres Schlafzimmer']
-];
+const galleries={
+  kilchberg:[
+    ['https://a0.muscache.com/im/pictures/hosting/Hosting-1640774749513849269/original/aaef4c64-34a1-4601-b5ea-7457669fb338.jpeg?im_w=1200','Wohnbereich'],
+    ['https://a0.muscache.com/im/pictures/hosting/Hosting-1640774749513849269/original/45ea1953-9627-4e41-840e-2433be61feb4.jpeg?im_w=1200','Essbereich'],
+    ['https://a0.muscache.com/im/pictures/hosting/Hosting-1640774749513849269/original/2455e184-3d05-4c2d-9c07-9c8064c618d5.jpeg?im_w=1200','Küche'],
+    ['https://a0.muscache.com/im/pictures/hosting/Hosting-1640774749513849269/original/41b3c36f-6c76-4afd-8bdb-e488a646fff0.png?im_w=1440','Schlafzimmer mit Seeblick'],
+    ['https://a0.muscache.com/im/pictures/hosting/Hosting-1640774749513849269/original/4b0a0c6d-473b-46a3-9b7b-62c8b3e9e60a.jpeg?im_w=1200','Weiteres Schlafzimmer']
+  ],
+  zurich:[
+    ['assets/zurich-studio-main.webp','Studio Gesamtansicht'],
+    ['assets/zurich-studio-tv.webp','Studio mit TV und Arbeitsfläche'],
+    ['assets/zurich-studio-bed.webp','Schlafbereich'],
+    ['assets/zurich-kitchen.webp','Küchenzeile'],
+    ['assets/zurich-bath.webp','Duschbad'],
+    ['assets/zurich-coffee.webp','Kaffee- und Küchenausstattung']
+  ]
+};
 const dialog=document.getElementById('lightbox');
 const dialogImage=document.getElementById('lightbox-image');
 const counter=document.getElementById('lightbox-counter');
 let active=0;
+let activeGallery='kilchberg';
 function showImage(index){
+  const gallery=galleries[activeGallery];
   active=(index+gallery.length)%gallery.length;
   dialogImage.src=gallery[active][0];
   dialogImage.alt=gallery[active][1];
@@ -26,7 +38,15 @@ function showImage(index){
 }
 document.querySelectorAll('[data-gallery]').forEach(btn=>{
   btn.addEventListener('click',()=>{
+    activeGallery='kilchberg';
     showImage(Number(btn.dataset.gallery));
+    dialog.showModal();
+  });
+});
+document.querySelectorAll('[data-zurich-gallery]').forEach(btn=>{
+  btn.addEventListener('click',()=>{
+    activeGallery='zurich';
+    showImage(Number(btn.dataset.zurichGallery));
     dialog.showModal();
   });
 });
